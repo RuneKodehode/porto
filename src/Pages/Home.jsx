35395e35c9c6e326f42312850e3f9e5223893fd0
RuneKodehode <rune.kodehode@gmail.com>
2023-02-06 10:12:31 +0100

    lol

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,11 +5,11 @@ export const Home = () => {
       <br></br>
       <p className="projects">
         Hi, my name is Rune. <br></br>
-        <br></br>I’m a 33 year old front-end developer, who’s currently
+        <br></br>I`m a 33 year old front-end developer, who`s currently
         participating in a 1 year front-end developer course called Kodehode
         organized by Jobloop AS. <br></br>
         <br></br>My background is 10 years in the onshore oil and gas industry,
-        but my passion has always been within tech. Currently I’m well versed in
+        but my passion has always been within tech. Currently I`m well versed in
         CSS, HTML and Javascript, and React.<br></br> <br></br>My goal is to
         become a full-stack developer at some point.
         <br></br>
