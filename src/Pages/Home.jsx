d98c995659f5f9838553c0f66cac19eba73fec77
RuneKodehode <rune.kodehode@gmail.com>
2023-02-07 13:37:35 +0100

    small edits

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,7 @@
 export const Home = () => {
   return (
     <div className="flavourText">
-      <h2>Welcome</h2>
+      <br />
       <p className="projects">
         I`m a 33 year old front-end developer, who`s currently participating in
         a 1 year front-end developer course organized by Jobloop AS. <br></br>
