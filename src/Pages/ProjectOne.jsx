35012ed8b7a6b9af847204a37084003f9ae6202a
RuneKodehode <rune.kodehode@gmail.com>
2023-02-07 09:18:17 +0100

    iframe size

--- src/Pages/ProjectOne.jsx
+++ src/Pages/ProjectOne.jsx
@@ -1,8 +1,6 @@
 export const ProjectOne = () => {
   return (
     <iframe
-      width={100}
-      height={100}
       src="https://runekodehode.github.io/Parallax/"
       frameBorder="0"
     ></iframe>
