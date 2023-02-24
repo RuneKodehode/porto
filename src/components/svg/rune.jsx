924ac4e5875960e8d1e7f3a3b71230fd25deebd0
RuneKodehode <rune.kodehode@gmail.com>
2023-02-24 09:34:43 +0100

    fixed small stuff. transition speed, margins etc.

--- src/components/svg/rune.jsx
+++ src/components/svg/rune.jsx
@@ -9,13 +9,12 @@ export const Rune = () => {
             width: 15px; 
             height: 5px;
             background-color: cyan;
-            box-shadow: 5px 5px 20px lightblue;
+            box-shadow: 5px 5px 20px green;
             animation: m 5s alternate-reverse infinite;
             transform: translateX(calc(@sin(@i / 1) * π * 4px));
             animation-delay: calc(
              -1s/@I * @i * @sin(@i)
              );
-             
              @keyframes m {
                10%, 20%, 100% { scale: 1; opacity: 1}
               10%, 70%, 50% { scale: 0; opacity: 0 }`}
