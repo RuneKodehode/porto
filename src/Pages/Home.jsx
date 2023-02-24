e0980e093305748728aaeff1ae6b3934dab3b4f8
RuneKodehode <rune.kodehode@gmail.com>
2023-02-24 10:18:35 +0100

    touched upon the about me text

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,13 +5,16 @@ export const Home = () => {
       <br />
       <br />
       <p className="projects">
-        I`m a 33 year old front-end developer, who`s currently participating in
-        a 1 year front-end developer course organized by Jobloop AS. <br></br>
-        <br></br>
-        I'm currently well versed in HTML, CSS, Javascript, Figma and React.
+        I`m a 33 year old front-end developer. <br /> Currently I'm well versed
+        in HTML, CSS, Javascript, Figma and React.
         <br />
         <br />
         My goal is to become a full-stack developer at some point.
+      </p>
+      <br />
+      <p className="projects">
+        About me, I've lived in Stavanger for about 10 years, and my hobbies
+        include reading books, watching football, DnD and PC-games.
       </p>
     </div>
   );
