e5949cbf0261056fe049157a60226fd1ee6e3b9e
RuneKodehode <rune.kodehode@gmail.com>
2023-03-02 10:59:03 +0100

    added more symbols

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,8 +1,16 @@
 export const Contact = () => {
   return (
     <div id="contact">
-      <h3>Tlf: +47 91992120</h3>
-      <h3>E-mail: RuneLaupsa@gmail.com</h3>
+      <h3>
+        {" "}
+        Tlf: <span style={{ color: "orangered" }}>+47 91992120</span>
+      </h3>
+      <h3>
+        E-mail: <span style={{ color: "orangered" }}>RuneLaupsa@gmail.com</span>
+      </h3>
+      <br />
+      <br />
+      <h3>CV can be provided upon request. </h3>
     </div>
   );
 };
