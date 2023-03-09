238479e5b655733a769d49f557d0a651f56df2ab
RuneKodehode <rune.kodehode@gmail.com>
2023-03-09 09:39:29 +0100

    remove br and replaced with flex and gap

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,34 +1,25 @@
 export const Home = () => {
   return (
-    <div className="flavourText">
-      <br />
-      <br />
-      <br />
-      <div className="portfolio"> </div>
-
-      <p className="projects">
-        I`m a 33 year old front-end developer. <br />
-        <br />
-        I'm currently participating in a front-end developer course hosted by
-        Jobloop in Stavanger.
-        <br />
-        <br /> Currently I'm well versed in{" "}
+    <p className="projects">
+      <div>
+        I`m a 33 year old front-end developer. I'm currently participating in a
+        front-end developer course hosted by Jobloop in Stavanger.{" "}
+      </div>
+      <div>
+        Currently I'm well versed in{" "}
         <span style={{ color: "orangered" }}> HTML</span>,{" "}
         <span style={{ color: "orangered" }}>CSS</span>,{" "}
         <span style={{ color: "orangered" }}>Javascript</span>,{" "}
         <span style={{ color: "orangered" }}>Figma</span> and
         <span style={{ color: "orangered" }}> React.</span>
-        <br />
-        <br />
-        My goal is to become a full-stack developer at some point.
-      </p>
-      <br />
-      <p className="projects">
+      </div>
+      <div>My goal is to become a full-stack developer at some point.</div>
+      <div>
         About me, I've lived in{" "}
         <span style={{ color: "orangered" }}> Stavanger</span> for about 10
         years, and my hobbies include reading books, watching football,{" "}
         <span style={{ color: "red" }}> D&D</span> and PC-games.
-      </p>
-    </div>
+      </div>
+    </p>
   );
 };
