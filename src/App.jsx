e9943ba781bd5d408cd81263c41baf9287ed307d
RuneKodehode <rune.kodehode@gmail.com>
2023-03-10 13:11:02 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { NavBar } from "./components/NavBar/NavBar.jsx";
 import { NavBarTwo } from "./components/NavBar/NavbarTwo.jsx";
 import { Home } from "./components/Pages/Home";
-import { ProjectOne } from "./components/Pages/ProjectOne";
-import { ProjectTwo } from "./components/Pages/ProjectTwo";
+import { ProjectOne } from "./components/Pages/ProjectOne.jsx";
+import { ProjectTwo } from "./components/Pages/ProjectTwo.jsx";
 import { AboutProjOne } from "./components/Pages/AboutProjOne.jsx";
 import { AboutProjTwo } from "./components/Pages/AboutProjTwo.jsx";
 import { AboutBreakout } from "./components/Pages/AboutBreakout.jsx";
@@ -15,11 +15,11 @@ function App() {
   return (
     <div className="">
       <Router>
-        <div className="">
+        <div className="grid sm:grid-rows-[15%_minmax(150px,_auto)_20px] grid-rows-[15%_minmax(150px,_auto)_150px] grid-cols-[7%_minmax(5px,_2fr)_7%] sm:grid-cols-[16%_minmax(5px,_2fr)_16%]  h-screen">
           <Rune />
           <NavBar />
           <NavBarTwo />
-          <div className="">
+          <div className="grid row-start-2 col-start-1 col-span-3 sm:col-span-1 sm:col-start-2 pb-2.5">
             <Routes className="flex">
               <Route path="/" element={<Home />} />
               <Route path="/ProjectOne" element={<ProjectOne />} />
